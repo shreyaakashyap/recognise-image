ae7b87a8236e0ea698775af3218bfa1756a9a744
Shreyaa Kashyap <44851128+shreyaakashyap@users.noreply.github.com>
2022-02-19 13:52:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+Webcam.attach('#camera');
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+
+function takeSnap() {
+   Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"';
+   }); 
+}
+
+console.log("ml5 version:", ml5.version);
+classifier=ml5.classifier("https://teachablemachine.withgoogle.com/models/6KpVQ1hPC/model.json", modelLoaded);
+function modelLoaded() {
+    console.log("model loaded");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("results_object_name").innerHTML = results[0].label;
+        document.getElementById("results_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
